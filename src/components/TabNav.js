c2b10f3cf615c76da4c29c7c44cf9a9742db7702
XAmi101 <famousvacancy@gmail.com>
2019-08-23 14:54:29 -0400

    MVP done, NAvBAr fixed

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,6 +2,7 @@ import React  from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import  "semantic-ui-css//semantic.min.css";
 // import { NavLink } from "react-router-dom";
+import WelcomePage from "./WelcomePage"
 import CharacterList from "./CharacterList"
 import LocationsList from "./LocationsList"
 import EpisodeList from "./EpisodeList"
@@ -11,6 +12,7 @@ import EpisodeList from "./EpisodeList"
 // https://react.semantic-ui.com/modules/tab/
 
 const panes = [
+        { menuItem: 'Home', render: () => <Tab.Pane><WelcomePage /></Tab.Pane> },
         { menuItem: 'Characters', render: () => <Tab.Pane><CharacterList /></Tab.Pane> },
         { menuItem: 'Locations', render: () => <Tab.Pane><LocationsList /></Tab.Pane> },
         { menuItem: 'Episodes', render: () => <Tab.Pane><EpisodeList /></Tab.Pane> },
