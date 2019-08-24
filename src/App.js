705513a53341153a701088e1845cb607d45dfdcc
XAmi101 <famousvacancy@gmail.com>
2019-08-24 08:21:14 -0400

    MVP, router FIxed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Route } from "react-router-dom";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-// import WelcomePage from "./components/WelcomePage.js";
-// import CharacterList from "./components/CharacterList.js";
-// import LocationsList from "./components/LocationsList.js";
-// import EpisodeList from "./components/EpisodeList";
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList.js";
+import LocationsList from "./components/LocationsList.js";
+import EpisodeList from "./components/EpisodeList";
 
 
 
@@ -16,11 +16,10 @@ export default function App() {
     <main>
       <Header />
       <TabNav />
-    {/*  <CharacterList />
         <Route path="/" exact component={WelcomePage} />
         <Route path="/character" component={CharacterList} />
         <Route path="/location" component={LocationsList} />
-        <Route path="/episode" component={EpisodeList} /> */}
+        <Route path="/episode" component={EpisodeList} />
 
     </main>
   );
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,22 +1,19 @@
-import React  from "react";
+import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
-import  "semantic-ui-css//semantic.min.css";
-// import { NavLink } from "react-router-dom";
-import WelcomePage from "./WelcomePage"
-import CharacterList from "./CharacterList"
-import LocationsList from "./LocationsList"
-import EpisodeList from "./EpisodeList"
+import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs
 // https://react.semantic-ui.com/modules/tab/
 
-const panes = [
-        { menuItem: 'Home', render: () => <Tab.Pane><WelcomePage /></Tab.Pane> },
-        { menuItem: 'Characters', render: () => <Tab.Pane><CharacterList /></Tab.Pane> },
-        { menuItem: 'Locations', render: () => <Tab.Pane><LocationsList /></Tab.Pane> },
-        { menuItem: 'Episodes', render: () => <Tab.Pane><EpisodeList /></Tab.Pane> },
-      ]
+export default function TabNav() {
+  return (
+    <div className='ui attached tabular menu '>
+         <NavLink exact to='/' className=' item'>Home Page</NavLink>
+         <NavLink to='/character' className='item'>Characters </NavLink>
+         <NavLink to='/location' className='item'>Locations </NavLink>
+         <NavLink to='/episode' className='item'>Episodes </NavLink>
+       </div>
 
-const TabNav = () => <Tab panes={panes} />
-export default TabNav
+ );
+ };
